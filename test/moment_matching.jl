9a79325c40bdbe6f2f21283ff623f6e7e83a9128
Kai Xu <xukai921110@gmail.com>
2019-09-12 22:10:12 +0100

    Add constraint tests for MMDNumerical (#4)

--- test/moment_matching.jl
+++ test/moment_matching.jl
@@ -1,6 +1,6 @@
-using Test, Distances, DensityRatioEstimation
+using Test, Statistics, Distributions, Distances, DensityRatioEstimation
 
-@testset "Correctness of `DensityRatioEstimation.pairwise_dot`" begin
+@testset "Correctness of `pairwise_dot`" begin
     pairwise_dot(x) = pairwise(SqEuclidean(), x; dims=2)
     pairwise_dot(x, y) = pairwise(SqEuclidean(), x, y; dims=2)
 
@@ -15,7 +15,7 @@ using Test, Distances, DensityRatioEstimation
         18.0 8.0  0.0
     ]
 
-    @testset "`DensityRatioEstimation.pairwise_dot(x)`" begin
+    @testset "`pairwise_dot(x)`" begin
         n_randtests = 10
         @test DensityRatioEstimation.pairwise_dot(xtest) == ytest
         for _ = 1:n_randtests
@@ -24,7 +24,7 @@ using Test, Distances, DensityRatioEstimation
         end
     end
 
-    @testset "`DensityRatioEstimation.pairwise_dot(x, y)`" begin
+    @testset "`pairwise_dot(x, y)`" begin
         n_randtests = 10
         @test DensityRatioEstimation.pairwise_dot(xtest) == ytest
         for _ = 1:n_randtests
@@ -32,5 +32,22 @@ using Test, Distances, DensityRatioEstimation
             yrand = randn(784, 200)
             @test DensityRatioEstimation.pairwise_dot(xrand, yrand) ≈ pairwise_dot(xrand, yrand)
         end
+    end
+end
+
+@testset "MMDNumerical`" begin
+    @testset "Positivity and normalisation constraints" begin
+        dist_nu = Normal(1, 1)
+        dist_de = Normal(0, 2)
+
+        n_nu = 100
+        x_nu = rand(dist_nu, 1, n_nu)
+        n_de = 200
+        x_de = rand(dist_de, 1, n_de)
+
+        r_numerical  = estimate_ratio(MMDNumerical(), x_de, x_nu)
+
+        @test mean(r_numerical) ≈ 1
+        @test all(r_numerical .> 0)
     end
 end
