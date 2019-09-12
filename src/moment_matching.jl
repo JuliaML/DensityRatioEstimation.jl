d7a3910482cfd0bdca754123cfa77a5cd987cd60
Kai Xu <xukai921110@gmail.com>
2019-09-12 21:31:33 +0100

    update constraint name

--- src/moment_matching.jl
+++ src/moment_matching.jl
@@ -48,7 +48,7 @@ function estimate_ratio(mmd::AbstractMMD, x_de, x_nu; σs=nothing)
 end
 
 @with_kw struct MMDNumerical <: AbstractMMD
-    positive::Bool=true
+    positivity::Bool=true
     normalisation::Bool=true
 end
 
@@ -57,7 +57,7 @@ function _estimate_ratio(mmd::MMDNumerical, Kdede, Kdenu)
     model = Model(with_optimizer(IpoptOptimizer; print_level=0))
     @variable(model, r[1:n_de])
     @objective(model, Min, 1 / n_de ^ 2 * sum(r[i] * Kdede[i,j] * r[j] for i = 1:n_de, j=1:n_de) - 2 / (n_de * n_nu) * sum(r[i] * Kdenu[i,j] for i = 1:n_de, j=1:n_nu))
-    if mmd.positive
+    if mmd.positivity
         @constraint(model, r .>= 0)
     end
     if mmd.normalisation
