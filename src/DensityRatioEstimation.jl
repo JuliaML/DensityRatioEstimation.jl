7b21a4332cdb4bcc646ba53c95214a75a97b4c89
Kai Xu <xukai921110@gmail.com>
2019-09-12 22:33:35 +0100

    remove estimate_ratio placeholder

--- src/DensityRatioEstimation.jl
+++ src/DensityRatioEstimation.jl
@@ -3,10 +3,9 @@ module DensityRatioEstimation
 using Parameters, Statistics, LinearAlgebra, JuMP
 import Ipopt; IpoptOptimizer = Ipopt.Optimizer
 
-estimate_ratio() = error("`estimate_ratio()` is not implemented.")
-export estimate_ratio
-
 include("moment_matching.jl")
 export MMDAnalytical, MMDNumerical
+
+export estimate_ratio
 
 end
