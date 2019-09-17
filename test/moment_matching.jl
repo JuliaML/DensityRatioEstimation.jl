2110f36912197d1b90f10c2cb646e86ce2aa4692
Kai Xu <xukai921110@gmail.com>
2019-09-17 13:54:27 +0100

    Add test for gaussian_gram (#6)

--- test/moment_matching.jl
+++ test/moment_matching.jl
@@ -1,8 +1,9 @@
 using Test, Statistics, Distributions, Distances, DensityRatioEstimation
+using DensityRatioEstimation: pairwise_dot, gaussian_gram, gaussian_gram_by_pairwise_dot
 
-@testset "Correctness of `pairwise_dot`" begin
-    pairwise_dot(x) = pairwise(SqEuclidean(), x; dims=2)
-    pairwise_dot(x, y) = pairwise(SqEuclidean(), x, y; dims=2)
+@testset "Correctness of `pairwise_dot` and `gaussian_gram_by_pairwise_dot`" begin
+    pairwise_dot_golden(x) = pairwise(SqEuclidean(), x; dims=2)
+    pairwise_dot_golden(x, y) = pairwise(SqEuclidean(), x, y; dims=2)
 
     xtest = [
         1.0 2.0 4.0; 
@@ -17,20 +18,29 @@ using Test, Statistics, Distributions, Distances, DensityRatioEstimation
 
     @testset "`pairwise_dot(x)`" begin
         n_randtests = 10
-        @test DensityRatioEstimation.pairwise_dot(xtest) == ytest
+        @test pairwise_dot(xtest) == ytest
         for _ = 1:n_randtests
             xrand = randn(784, 100)
-            @test DensityRatioEstimation.pairwise_dot(xrand) ≈ pairwise_dot(xrand)
+            @test pairwise_dot(xrand) ≈ pairwise_dot_golden(xrand)
         end
     end
 
     @testset "`pairwise_dot(x, y)`" begin
         n_randtests = 10
-        @test DensityRatioEstimation.pairwise_dot(xtest) == ytest
+        @test pairwise_dot(xtest, xtest) == ytest
         for _ = 1:n_randtests
             xrand = randn(784, 100)
             yrand = randn(784, 200)
-            @test DensityRatioEstimation.pairwise_dot(xrand, yrand) ≈ pairwise_dot(xrand, yrand)
+            @test pairwise_dot(xrand, yrand) ≈ pairwise_dot_golden(xrand, yrand)
+        end
+    end
+
+    @testset "`gaussian_gram`" begin
+        for σ in sqrt.([1, 2, 4, 8, 16])
+            K1 = gaussian_gram(xtest, σ)
+            K2 = gaussian_gram(xtest, xtest, σ)
+            K3 = gaussian_gram_by_pairwise_dot(ytest, σ)
+            @test K1 == K2 == K3 == exp.(-ytest / (2 * σ ^ 2))
         end
     end
 end
