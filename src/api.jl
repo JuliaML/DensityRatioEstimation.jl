d411349db5c6413ec2b9ab6981144be6e842a09c
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-12-07 14:28:41 -0300

    Add DensityRatioTypes

--- src/api.jl
+++ src/api.jl
@@ -3,6 +3,42 @@
 # ------------------------------------------------------------------
 
 """
+    DensityRatio
+
+A ratio of two probability density functions.
+"""
+abstract type DensityRatio end
+
+"""
+    evaluate(r, x)
+
+Evaluate density ratio `r` at point `x`.
+"""
+evaluate(r::DensityRatio, x::AbstractVector) = @error "not implemented"
+
+"""
+    DiscreteDensityRatio(points, ratios)
+
+A ratio of two probability density functions for
+a discrete set of `points`. `ratios` is a vector
+of values for the `points`.
+"""
+struct DiscreteDensityRatio{P<:AbstractVector,V<:AbstractVector} <: DensityRatio
+  points::P
+  ratios::V
+end
+
+"""
+    FunctionalDensityRatio(func)
+
+A ratio of two probability density functions given in
+functional form `func`.
+"""
+struct FunctionalDensityRatio{F<:Function} <: DensityRatio
+  func::F
+end
+
+"""
     DensityRatioEstimator
 
 A method for density ratio estimation.
