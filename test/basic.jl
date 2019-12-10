dae6a840c71ce3daf2eb17aa075bf7e90131eada
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-12-10 17:00:29 -0300

    Update tests

--- test/basic.jl
+++ test/basic.jl
@@ -1,11 +1,14 @@
 @testset "Basic" begin
   for (d_nu, d_de) in [pair₁, pair₂]
     Random.seed!(123)
-    x_nu, x_de = rand(d_nu, 1000), rand(d_de, 1500)
-    for (dre, optlib) in [(KMM(),   JuMPLib),
-                          (KLIEP(), OptimLib),
-                          (KLIEP(), ConvexLib)]
+    x_nu, x_de = rand(d_nu, 100), rand(d_de, 200)
+    @testset "$dre -- $optlib" for (dre, optlib) in [(KMM(),   JuMPLib),
+                                                     (KLIEP(), OptimLib),
+                                                     (KLIEP(), ConvexLib)]
+
       r = densratio(x_nu, x_de, dre, optlib=optlib)
+
+      # density ratios must be positive
       @test all(r .> 0)
     end
   end
