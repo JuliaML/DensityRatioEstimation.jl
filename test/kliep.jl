39eefd17c3e299cfd0dd13a144dbb2ccc0b0e425
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-12-10 22:07:00 -0300

    Add tests for KLIEP

--- test/kliep.jl
+++ test/kliep.jl
@@ -1,2 +1,16 @@
 @testset "KLIEP" begin
+  for (i, pair) in enumerate([pair₁, pair₂])
+    d_nu, d_de = pair
+    Random.seed!(123)
+    x_nu, x_de = rand(d_nu, 2000), rand(d_de, 1000)
+
+    # estimated density ratio
+    σ, b = 1.0, 100
+    r̂ = densratio(x_nu, x_de, KLIEP(σ, b))
+
+    if visualtests
+      gr(size=(800,800))
+      @plottest plot_d_nu(pair,x_de,r̂) joinpath(datadir,"KLIEP-$i.png") !istravis
+    end
+  end
 end
