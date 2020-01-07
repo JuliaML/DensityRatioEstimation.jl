7f6162ead070b3921137201671ecc7ae36203761
Kai Xu <xukai921110@gmail.com>
2020-01-07 14:49:00 +0000

    recover the correctness check for mixture case for kleip

--- test/kliep.jl
+++ test/kliep.jl
@@ -1,5 +1,5 @@
 @testset "KLIEP -- $optlib" for optlib in [OptimLib, ConvexLib]
-  for (i, pair) in enumerate([pair₁, pair₂])
+  for (i, pair, rtol_correctness) in zip(1:2, [pair₁, pair₂], (2e-1, 4e-1))
     d_nu, d_de = pair
     Random.seed!(123)
     x_nu, x_de = rand(d_nu, 1_000), rand(d_de, 500)
@@ -15,11 +15,8 @@
     @test abs(mean(r̂) - 1) ≤ 1e-2
     @test all(r̂ .≤ Inf)
 
-    if i == 1 # FIXME: only check correctness for the Gaussian case now
-      # compare against true ratio
-      r = pdf.(d_nu, x_de) ./ pdf.(d_de, x_de)
-      @test r ≈ r̂ rtol=2e-1
-    end
+    r = pdf.(d_nu, x_de) ./ pdf.(d_de, x_de)
+    @test r ≈ r̂ rtol=rtol_correctness
 
     if visualtests
       gr(size=(800, 800))
