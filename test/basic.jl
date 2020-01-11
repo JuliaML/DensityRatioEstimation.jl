649cfde7c4ae26b4fc62bd87a421dc8895e72d8f
Kai Xu <xukai921110@gmail.com>
2020-01-11 02:49:59 +0000

    revert basic.jl

--- test/basic.jl
+++ test/basic.jl
@@ -8,5 +8,26 @@
     G = DensityRatioEstimation.gaussian_gramian(x_nu, x_nu, σ=2.0)
     @test issymmetric(G)
     @test all(G .> 0)
+
+    # features can be any indexable
+    x_nu = [(a=1.,b=2.),(a=3.,b=4.)]
+    x_de = [(a=1.,b=2.),(a=3.,b=4.),(a=5.,b=6.)]
+    G = DensityRatioEstimation.gaussian_gramian(x_nu, x_de)
+    @test size(G) == (2, 3)
+    @test all(G .> 0)
+  end
+
+  for (d_nu, d_de) in [pair₁, pair₂]
+    Random.seed!(123)
+    x_nu, x_de = rand(d_nu, 100), rand(d_de, 200)
+    @testset "$dre -- $optlib" for (dre, optlib) in [(KMM(),   JuMPLib),
+                                                     (KLIEP(), OptimLib),
+                                                     (KLIEP(), ConvexLib)]
+
+      r = densratio(x_nu, x_de, dre, optlib=optlib)
+
+      # density ratios must be positive
+      @test all(r .> 0)
+    end
   end
 end
