f258cb2a3bc620246e7188f61cd8f80477fae68d
Kai Xu <xukai921110@gmail.com>
2020-01-15 17:36:33 +0000

    fix KMM test

--- test/kmm.jl
+++ test/kmm.jl
@@ -1,22 +1,21 @@
 @testset "KMM -- $optlib" for optlib in [JuliaLib, JuMPLib]
-  for (i, pair) in enumerate([pair₁, pair₂])
+  for (i, pair, rtol) in [(1, pair₁, 2e-1), (2, pair₂, 4e-1)]
     d_nu, d_de = pair
     Random.seed!(123)
     x_nu, x_de = rand(d_nu, 2_000), rand(d_de, 1_000)
 
     # estimated density ratio
-    σ, B, ϵ, λ = 1.5, Inf, 0.01, 0.001
+    D = [sqrt(DensityRatioEstimation.euclidsq(x, y)) for x in x_nu, y in x_de]
+    σ, B, ϵ, λ = Distributions.mode(D), Inf, 0.001, 0.01
     r̂ = densratio(x_nu, x_de, KMM(σ=σ, B=B, ϵ=ϵ,  λ=λ), optlib=optlib)
 
     # simplex constraints
     @test abs(mean(r̂) - 1) ≤ 1e-2
     @test all(r̂ .≤ B)
 
-    if i == 1 # FIXME: only check correctness for the Gaussian case now
-      # compare against true ratio
-      r = pdf.(d_nu, x_de) ./ pdf.(d_de, x_de)
-      @test r ≈ r̂ rtol=2e-1
-    end
+    # compare against true ratio
+    r = pdf.(d_nu, x_de) ./ pdf.(d_de, x_de)
+    @test r ≈ r̂ rtol=rtol
 
     if visualtests
       gr(size=(800, 800))
