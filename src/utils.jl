0f25e197e7c3b879e422089f5ec6aceae7d7d142
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-01-21 22:08:28 -0300

    Remove trivial sample_centers function

--- src/utils.jl
+++ src/utils.jl
@@ -18,15 +18,3 @@ kernel with bandwidth `σ`.
 function gaussian_gramian(xs, ys; σ=1)
   [exp(-euclidsq(x, y) / 2σ^2) for x in xs, y in ys]
 end
-
-"""
-    sample_centers(x_nu, nmax)
-
-Return the indices of `x_nu` used as the kernel centers
-in kernel approximation of density ratio function. The
-parameter `nmax` is the maximum number of indices to select.
-"""
-function sample_centers(x_nu, nmax::Int)
-  n = length(x_nu)
-  sample(1:n, min(n, nmax), replace=false)
-end
