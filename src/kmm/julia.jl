921cb3834f0866f7a23da45ab7480b817cd66951
Kai Xu <xukai921110@gmail.com>
2020-02-10 21:44:49 +0000

    provide low-level interface

--- src/kmm/julia.jl
+++ src/kmm/julia.jl
@@ -17,13 +17,16 @@ function _densratio(x_nu, x_de, dre::KMM{T}, optlib::Type{JuliaLib}) where {T}
   # warn ignored parameters
   warn_kmm_julialib(B, ϵ)
 
-  # number of numerator and denominator samples
-  n_nu, n_de = length(x_nu), length(x_de)
-
   # Gramian matrices for numerator and denominator
   Kdede = gaussian_gramian(x_de; σ=σ)
   Kdenu = gaussian_gramian(x_de, x_nu; σ=σ)
 
   # closed-form solution (without constraints)
+  _densratio_kmm(Kdede, Kdenu, dre.λ)
+end
+
+# Interface to deal with an array of σs
+function _densratio_kmm(Kdede, Kdenu, λ::T) where {T<:AbstractFloat}
+  n_de, n_nu = size(Kdenu)
   T(n_de / n_nu) * ((Kdede + λ*I) \ vec(sum(Kdenu, dims=2)))
 end
--- src/utils.jl
+++ src/utils.jl
@@ -9,6 +9,24 @@ Return the Euclidean distance between two indexable objects.
 """
 euclidsq(x, y) = sum((x[i] - y[i])^2 for i in eachindex(x))
 
+# Support matrix data in a GPU and AD compatible way
+
+function euclidsq(x::T) where {T<:AbstractMatrix}
+    n = size(x, 2)
+    xixj = transpose(x) * x
+    xsq = sum(x .^ 2; dims=1)
+    transpose(xsq) .+ xsq - 2xixj
+end
+
+function euclidsq(x::T, y::T) where {T<:AbstractMatrix}
+    nx = size(x, 2)
+    ny = size(y, 2)
+    xiyj = transpose(x) * y
+    xsq = sum(x .^ 2; dims=1)
+    ysq = sum(y .^ 2; dims=1)
+    transpose(xsq) .+ ysq - 2xiyj
+end
+
 """
     gaussian_gramian(xs, ys, σ=1)
 
@@ -18,3 +36,9 @@ kernel with bandwidth `σ`.
 gaussian_gramian(xs; kwargs...) = gaussian_gramian(xs, xs; kwargs...)
 gaussian_gramian(xs, ys; σ=1) =
   [exp(-euclidsq(x, y) / 2σ^2) for x in xs, y in ys]
+
+function gaussian_gramian(X::T, Y::T; σ=1) where {T<:AbstractMatrix}
+    gaussian_gramian_by_euclidsq(euclidsq(X, Y), σ)
+end
+
+gaussian_gramian_by_euclidsq(dsq, σ) = exp.(-dsq ./ 2σ^2)
