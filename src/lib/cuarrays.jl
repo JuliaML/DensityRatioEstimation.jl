b73c959c33e494d06c31b1628b7bf4cb1266f064
Kai Xu <xukai921110@gmail.com>
2020-02-12 17:18:37 +0000

    make safe_diagm type consistent to lambda

--- src/lib/cuarrays.jl
+++ src/lib/cuarrays.jl
@@ -1,4 +1,4 @@
 import .CuArrays
 
-# Aovid `Kdede + λ * I` with CuArrays which involes scalar operations and is slow
-safe_diagm(Kdede::T, λ) where {T<:CuArrays.CuArray} = T(diagm(0 => fill(λ, size(Kdede, 1)))) 
+# Aovid `mat + a * I` with CuArrays which involes scalar operations and is slow
+safe_diagm(mat::CuArrays.CuArray, a::T) where {T} = CuArrays.CuArray{T}(diagm(0 => fill(a, size(mat, 1))))
