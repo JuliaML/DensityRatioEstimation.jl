866e68a9ae9ac264a084eaa78fd55675e350e78c
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-11-13 10:36:24 -0300

    ISC == MIT

--- src/utils/zygote.jl
+++ src/utils/zygote.jl
@@ -1,3 +1,7 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENSE in the project root.
+# ------------------------------------------------------------------
+
 import .Zygote
 
 Zygote.@nograd warn_kmm_julialib
