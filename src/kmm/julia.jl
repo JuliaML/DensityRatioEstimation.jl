98533742b871a24cf05cb33e229f1679dfec9a8b
Kai Xu <5985769+xukai92@users.noreply.github.com>
2022-06-13 15:38:07 +0100

    feat: split KMM into KMM and uKMM (#37) (#44)
    * feat: split KMM into KMM and uKMM (#37)
    
    * chore: remove @nograd for removed function
    
    Co-authored-by: Kai Xu <contact@xuk.ai>

--- src/kmm/julia.jl
+++ src/kmm/julia.jl
@@ -2,20 +2,7 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-# NOTE: this function is a hack for Zygote.jl compatbility; see lib/zygote.jl
-function warn_kmm_julialib(B, ϵ)
-  # warn user that closed-form solution does
-  # not consider probability simplex constraints
-  isinf(B) || @warn "B parameter ignored when optlib=JuliaLib"
-  iszero(ϵ) || @warn "ϵ parameter ignored when optlib=JuliaLib"
-end
-
-function _kmm_ratios(K, κ, dre::KMM, optlib::Type{JuliaLib})
-  # retrieve parameters
-  @unpack B, ϵ = dre
-
-  warn_kmm_julialib(B, ϵ) # warn ignored parameters
-
-  # density ratio via solve
+function _kmm_ratios(K, κ, dre::uKMM, optlib::Type{JuliaLib})
+  # density ratio via solver
   K \ vec(κ)
 end
